fbeb60b8a4ef3e64161594d9e33e18ae0c6427f2
edward-ke <kbinran@gmail.com>
2021-02-23 18:06:20 +0800

    修改MainActivity内容

--- app/src/main/java/com/example/edwarddemo/MainActivity.java
+++ app/src/main/java/com/example/edwarddemo/MainActivity.java
@@ -1,4 +1,14 @@
 package com.example.edwarddemo;
 
-public class MainActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 }
